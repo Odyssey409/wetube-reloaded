f81c8f08570c3b2e88c504deb80c04e0313eab17
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-01 17:47:36 +0900

    youtube_clone_lecture_Day_3(#3.5~#3.10))

--- src/server.js
+++ src/server.js
@@ -4,18 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (requests, responses, next) => {
+  console.log(`${requests.method} ${requests.url}`);
+  next();
+};
+
 const handleHome = (requests, responses) => {
   return responses.send("Hi!");
 };
 
-const handleLogin = (requests, responses) => {
-  return responses.send("Login here");
-};
-
 app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
