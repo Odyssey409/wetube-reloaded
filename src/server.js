c5aa4b3096edba5031e51d8ed2546a08e408fd6b
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-02 02:02:57 +0900

    youtube_clone_lecture_Routers_UglyCode_ver(#4.0~#4.1)

--- src/server.js
+++ src/server.js
@@ -6,17 +6,29 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const login = (requests, responses, next) => {
-  console.log(`${requests.method} ${requests.url}`);
-  next();
-};
-
-const handleHome = (requests, responses) => {
-  return responses.send("Hi!");
-};
-
 app.use(logger);
-app.get("/", handleHome);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
