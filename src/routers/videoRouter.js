a30be7523ef38a412de2c915e2a4f539dc53331b
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-02 23:21:44 +0900

    youtube_clone_lecture_Day_5_routerExport_parameters(~#4.8))

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see); // (\\d+) is a kind of regular expression(정규식) and that regular expression means it will only take numbers
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
