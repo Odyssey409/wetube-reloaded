63e9f0bc135db40e09150125b800994380ccb268
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-07 02:03:14 +0900

    youtube_clone_lecture_pug_mixins(#5.9)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 4,
+      comments: 3,
+      createdAt: "5 minutes ago",
+      views: 100,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 3,
+      comments: 11,
+      createdAt: "3 minutes ago",
+      views: 70,
+      id: 3,
+    },
+  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) =>
