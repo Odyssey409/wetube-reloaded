6702178b31c2a48b374333847a6b8c6cad403a78
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-08 01:50:09 +0900

    youtube_clone_lecture_pug_array_database(#6.0~#6.1)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,39 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 4,
+    comments: 3,
+    createdAt: "5 minutes ago",
+    views: 100,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 3,
+    comments: 11,
+    createdAt: "3 minutes ago",
+    views: 70,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 4,
-      comments: 3,
-      createdAt: "5 minutes ago",
-      views: 100,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 3,
-      comments: 11,
-      createdAt: "3 minutes ago",
-      views: 70,
-      id: 3,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) =>
-  res.render("watch", { pageTitle: "Watch Video" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see); // (\\d+) is a kind of regular expression(정규식) and that regular expression means it will only take numbers
+videoRouter.get("/:id(\\d+)", watch); // (\\d+) is a kind of regular expression(정규식) and that regular expression means it will only take numbers
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
