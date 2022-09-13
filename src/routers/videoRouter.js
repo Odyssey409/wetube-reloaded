8c16a6ca9f543e8a8122e0fa136a581f68814bcf
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-14 00:23:44 +0900

    youtube_clone_lecture_pug_editing_video(#6.2~#6.3)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,15 @@
 import express from "express";
 import {
   watch,
-  edit,
+  getEdit,
+  postEdit,
   upload,
   deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch); // (\\d+) is a kind of regular expression(정규식) and that regular expression means it will only take numbers
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
