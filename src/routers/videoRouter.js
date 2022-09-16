46b6a2250031d874f9035c4c7208d5c8ec66a5cc
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2022-09-17 00:18:43 +0900

    youtube_clone_lecture_mongo_video_details(#6.19)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,8 +9,8 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch); // (\\d+) is a kind of regular expression(정규식) and that regular expression means it will only take numbers
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch); // (\\d+) is a kind of regular expression(정규식) and that regular expression means it will only take numbers
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
